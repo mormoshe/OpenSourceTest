d37f1003c76acd47e84997f23fdf1a86abe6cf8b
mormoshe <mor>
2020-07-08 16:54:13 +0300

    add color picker

--- app/src/main/java/com/example/testproject/MainActivity.java
+++ app/src/main/java/com/example/testproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.testproject;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
@@ -11,27 +12,32 @@ import android.widget.Button;
 
 import ja.burhanrashid52.photoeditor.PhotoEditor;
 import ja.burhanrashid52.photoeditor.PhotoEditorView;
+import me.jfenn.colorpickerdialog.dialogs.ColorPickerDialog;
+import me.jfenn.colorpickerdialog.interfaces.OnColorPickedListener;
 
 public class MainActivity extends AppCompatActivity {
     private PhotoEditor mPhotoEditor;
+    private int currentColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        currentColor = Color.BLACK;
+        getSupportActionBar().setTitle("Paint");
         PhotoEditorView mPhotoEditorView = findViewById(R.id.photoEditorView);
         mPhotoEditor = new PhotoEditor.Builder(this, mPhotoEditorView)
                 .setPinchTextScalable(true)
                 .build();
         setButtonListeners();
+
     }
 
     private void setButtonListeners() {
         Button drawButton = findViewById(R.id.drawButton);
         Button eraseButton = findViewById(R.id.eraseButton);
-        Button blackButton = findViewById(R.id.blackButton);
-        Button redButton = findViewById(R.id.redButton);
+        final Button colorButton = findViewById(R.id.colorPicker);
+        colorButton.setBackgroundColor(currentColor);
         mPhotoEditor.setBrushSize(10);
         drawButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,16 +51,21 @@ public class MainActivity extends AppCompatActivity {
                 mPhotoEditor.brushEraser();
             }
         });
-        blackButton.setOnClickListener(new View.OnClickListener() {
+        colorButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mPhotoEditor.setBrushColor(Color.BLACK);
-            }
-        });
-        redButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mPhotoEditor.setBrushColor(Color.RED);
+                new ColorPickerDialog()
+                        .withColor(currentColor) // the default / initial color
+                        .withListener(new OnColorPickedListener<ColorPickerDialog>() {
+                            @Override
+                            public void onColorPicked(@Nullable ColorPickerDialog dialog, int color) {
+                                // a color has been picked; use it
+                                currentColor = color;
+                                mPhotoEditor.setBrushColor(color);
+                                colorButton.setBackgroundColor(color);
+                            }
+                        })
+                        .show(getSupportFragmentManager(), "colorPicker");
             }
         });
     }
